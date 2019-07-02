8878925f9545dde1429f34455d32d165ba4fc3d7
김장훈 <homekeeper@gdnet.co.kr>
2019-07-02 09:58:34 +0900

    [new] 로그인 새로 작성

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,35 +1,65 @@
 <template>
     <div class='pg-login-form'>
-        <input class='form-id' type='text' placeholder="Your ID" v-model="LoginForm.id">
+        <input data-cy='loginId' class='form-id' type='text' placeholder="Your ID" v-model="LoginForm.id">
         <br>
-        <input class='form-pw' type='text' placeholder="Your PW" v-model="LoginForm.pw">
+        <input data-cy='loginPw' class='form-pw' type='text' placeholder="Your PW" v-model="LoginForm.pw">
         <br>
-        <button @click='PostLogin'>Login</button>
+        <p data-cy='loginWordAlert' v-if='wordAlert'>You should input id, and pw, not blank</p>
+        <button data-cy='loginBtn' @click='PostLogin'>Login</button>
         <button>Cancel</button>
+        <p data-cy='loginRes'>{{resData}}</p>
     </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
     data(){
         return{
             LoginForm:{
                 id:'',
                 pw:''
-            }
+            },
+            wordAlert:false,
+            resData:''
         }
     },
     methods:{
         PostLogin(){
             let res = this.MatchInfo(this.LoginForm)
-            alert(res)
         },
         MatchInfo(LoginForm){
             let id = LoginForm.id
             let pw = LoginForm.pw
-            if(id ==='guardian' && pw==='guardian'){
-                return true
+            this.validateForm(id, pw, this.getHttp)
+        },
+        validateForm(id, pw, f){
+            if(id.length ==0 || pw.length ==0){
+                this.wordAlert = true
+                return false
             }
-            return false
+            f()
+            return true
+        },
+        goHome(){
+            this.$router.push({
+            name: 'about'
+        })
+        },
+        getHttp(){
+            // const baseURI = 'https://jsonplaceholder.typicode.com';
+            axios({
+                method: 'POST',
+                url : '/users',
+                // params : {
+                //     page : 1,
+                //     pagesize : 5
+                // }
+            }).then((response) => {
+                this.resData = response.data.data;
+                this.goHome()
+            }).catch((ex)=> {
+                console.log("ERR!!!!! : ", ex)
+            })
         }
     }
 }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="pg-login">
-        <p>This is login page</p>
+    <div class="pg-login" data-cy='loginHeadInfo'>
+        <p>Hellow This is Kims Blog</p>
+        <p>WelCome nice o meet you</p>
+        <p class='text-midterm'>This is from callifornia</p>
         <login-form></login-form>
     </div>
 </template>
